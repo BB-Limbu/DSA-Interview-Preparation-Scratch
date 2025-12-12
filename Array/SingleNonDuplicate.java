4749bc6b5befb05d6a42209095259c91c416d6ed
BB-Limbu <balbahadurlimbu404@gmail.com>
2025-12-12 15:57:32 +0530

    Finding Single Element

---
+++ Array/SingleNonDuplicate.java
@@ -0,0 +1,77 @@
+public class SingleNonDuplicate {
+    /*
+    Brute Force Approach
+    Time Complexity O(n)
+    Space Complexity O(1)
+     */
+
+    public static int singleNonDuplicate(int[] nums){
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        for(int i = 0; i < n; i++){
+            if(i == 0) {
+                if (nums[i] != nums[i + 1])
+                    return nums[i];
+            }
+            else if(i == n -1){
+                if(nums[i] != nums[i -1])
+                    return nums[i];
+            }
+            else{
+                if(nums[i] != nums[i + 1] && nums[i] != nums[i - 1])
+                    return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    /*
+    Brute Force Approach 2
+    Time Complexity O(n)
+    Space Complexity O(1)
+     */
+
+    public static int singleNonDuplicate1(int[] nums){
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+
+    /*
+    Optimal Approach
+    Time Complexity  O(log n)
+    Space Complexity O(1)
+     */
+
+    public static int singleNonDuplicate2(int[] nums){
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1] != nums[n -2])
+            return nums[n - 1];
+
+        int low = 1, high = n-2;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+                return nums[mid];
+            }
+
+            if((mid % 2 == 1 && nums[mid-1] == nums[mid]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])){
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
+
