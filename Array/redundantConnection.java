446c0462c6c7de6d5aaa39e27dcdfc72c6db5997
BB-Limbu <balbahadurlimbu404@gmail.com>
2025-12-23 22:30:02 +0530

    Redundant Connection

---
+++ Array/redundantConnection.java
@@ -0,0 +1,30 @@
+public class redundantConnection {
+    public static int[] findRedundantConnection(int[][] edges){
+
+        int[] parent = new int[edges.length + 1];
+        for(int i = 0; i < edges.length; i++){
+            parent[i] = i;
+        }
+        for(int[] edge : edges){
+            int node1 = edge[0];
+            int node2 = edge[1];
+            int root1 = find(parent, node1);
+            int root2 = find(parent, node2);
+
+            if(node1 == node2){
+                return edge;
+            }
+            parent[root2] = root1;
+        }
+        return new int[0];
+    }
+    private static int find(int[] parent, int node){
+        while(node != parent[node]){
+            parent[node] = parent[parent[node]];
+            node = parent[node];
+        }
+        return node;
+    }
+
+}
+
