055c93563f6b4a48949d923fe28b8176600bef03
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-01-08 23:25:02 +0530

    Column Title

---
+++ Array/ExcelSheet_columnTitle.java
@@ -0,0 +1,14 @@
+public class ExcelSheet_columnTitle {
+    public static String converToTile(int s){
+        StringBuilder res = new StringBuilder();
+
+        while(s > 0){
+            s--;
+            int rem = s % 26;
+            char letter = (char) ('A' + rem);
+            res.append(letter);
+            s /= 26;
+        }
+        return res.reverse().toString();
+    }
+}
