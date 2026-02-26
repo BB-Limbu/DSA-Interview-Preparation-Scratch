89f132969647d5ddd20aaa23a294c5a0eafb532b
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-02-26 22:31:42 +0530

    String Compression

---
+++ Array/String/CompressString.java
@@ -0,0 +1,19 @@
+public class CompressString {
+    public static String stringCompression(String str){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1){
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+}
+
