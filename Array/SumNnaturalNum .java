d865a8b67e9927729e3bbd345613711fcd64139e
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-03 22:19:52 +0530

    Sum Natural Number

---
+++ Array/SumNnaturalNum .java
@@ -0,0 +1,12 @@
+ class SumNnaturalNum {
+    public static int sumNaturalNum(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n + sumNaturalNum(n-1);
+    }
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(sumNaturalNum(n));
+    }
+}
