854617501cd433b396f15fc62d4ddd6700217335
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-14 22:20:28 +0530

    Duplicate Parenthesis

---
+++ Array/String/isDuplicateParenthesis.java
@@ -0,0 +1,33 @@
+package String;
+
+
+public class isDuplicateParenthesis {
+    /*
+    optimal Approach
+    Time Complexity O(n)
+    Space Complexity O(n)
+     */
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            // closing
+            if(ch == ')'){
+                int count = 0;
+                while(s.pop() != '('){
+                    count++;
+                }
+                if(count < 1){
+                    return true;
+                }
+
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+}
+
