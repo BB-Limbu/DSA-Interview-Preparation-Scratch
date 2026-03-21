e26110ee4ccf45ad041441444c151f447e872194
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-21 22:04:51 +0530

    Job Sequence

---
+++ Array/JobSequence.java
@@ -0,0 +1,35 @@
+
+
+public class JobSequence {
+    static class Job{
+        int deadline;
+        int profit;
+        int id;
+
+        public Job(int i, int d, int p){
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+
+    public static List<Integer> maxProfit(int[][] nums){
+        ArrayList<Job> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            list.add(new Job(i,nums[i][0], nums[i][1]));
+        }
+        //Applying sorting on object
+        Collections.sort(list, (obj1, obj2) -> obj2.profit - obj1.profit);//descending order in profit
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for(int i = 0; i< list.size(); i++){
+            Job curr = list.get(i);
+            if(curr.deadline > time){
+                seq.add(curr.id);
+                time++;
+            }
+        }
+        return seq;
+    }
+}
