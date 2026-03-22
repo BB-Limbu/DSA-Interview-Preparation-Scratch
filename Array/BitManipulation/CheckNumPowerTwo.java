2e55cfc67587f29a252d8d30b6d56100bfedd01e
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-22 22:11:56 +0530

    Check Number Power Two

---
+++ Array/BitManipulation/CheckNumPowerTwo.java
@@ -0,0 +1,16 @@
+public class CheckNumPowerTwo {
+    /*
+    Time Complexity O(n)
+    Space Complexity O(1)
+     */
+    public static boolean checkNumPowTwo(int n){
+        if((n & n-1) == 0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        int n = 9;
+        System.out.println(checkNumPowTwo(n));
+    }
+}
