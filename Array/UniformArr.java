3c27f5c09739864467b82a3722bffd2896a7561e
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-24 22:06:17 +0530

    Construct Uniform Parity Array

---
+++ Array/UniformArr.java
@@ -0,0 +1,33 @@
+
+
+public class UniformArr {
+    public static boolean uniformArr(int[] nums){
+        Arrays.sort(nums);
+
+        int min = nums[0];
+        //Even Case
+        if((min & 1) == 0){
+            int smallestOdd = Integer.MAX_VALUE;
+
+            for(int num : nums){
+                if((num & 1) == 1){
+                    if(smallestOdd == Integer.MAX_VALUE){
+                        smallestOdd = num;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        //Odd case
+        else{
+            for(int num : nums){
+                if((num & 1) == 0 && num <= min){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
