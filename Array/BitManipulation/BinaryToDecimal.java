703889c41410ccfa37149184fba4102d3528d993
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-25 22:37:40 +0530

    Binary To Decimal

---
+++ Array/BitManipulation/BinaryToDecimal.java
@@ -0,0 +1,20 @@
+
+
+public class BinaryToDecimal {
+    public static int binaryToDecimal(String nums){
+        int res = 0;
+        int power = 0;
+
+        for(int i = nums.length()-1; i >= 0; i--){
+            if(nums.charAt(i) == '1'){
+                res += Math.pow(2, power);
+            }
+            power++;
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        String str  = " 111";
+        System.out.println(binaryToDecimal(str));
+    }
+}
