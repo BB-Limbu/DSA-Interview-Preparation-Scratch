450f50430d8ca7d74f0790099c7446b2cd6fe8ea
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-03-27 22:54:58 +0530

    Minimum Bits Flip

---
+++ Array/BitManipulation/MinBitsFlip.java
@@ -0,0 +1,21 @@
+
+
+public class MinBitsFlip {
+    /*
+    Q. Minimum bits flips to convert Number
+    Time Complexity O(n)
+    Space Complexity O(1)
+     */
+    public static int minBitsFlip(int start, int goal){
+        int ans = start  ^ goal;
+        int count = 0;
+
+        for(int i = 0; i < 32; i++){
+            count += (ans & 1);
+            ans = ans >> 1;
+        }
+        return count;
+    }
+}
+
+
