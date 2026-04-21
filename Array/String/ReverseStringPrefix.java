67ef708f2bc859784c96e8ed460521a5d2813704
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-04-21 21:10:52 +0530

    Reverse String prefix

---
+++ Array/String/ReverseStringPrefix.java
@@ -0,0 +1,15 @@
+
+
+public class ReverseStringPrefix {
+    public static String reverseStrPrefix(String s, int k){
+        if(k > s.length()){
+            return "Invalid";
+        }
+        String firstHalf = s.substring(0, k);
+        String lastHalf = s.substring(k);
+
+        String reverse = new StringBuilder(firstHalf).reverse().toString();
+        return reverse + lastHalf;
+    }
+}
+
