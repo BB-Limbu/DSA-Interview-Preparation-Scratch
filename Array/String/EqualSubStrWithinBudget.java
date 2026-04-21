0d22910c4268cc1b832b5d36ce685cb2bd441d07
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-04-21 21:15:01 +0530

    Equal SubString within  Budget

---
+++ Array/String/EqualSubStrWithinBudget.java
@@ -0,0 +1,23 @@
+
+
+public class EqualSubStrWithinBudget {
+    /*
+    Get Equal Substrings Within Budget
+    Using sliding window
+     */
+    public static int equalSubStrBudget(String s, String t, int maxCost){
+        int left = 0, maxLen = 0, cost = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            cost += Math.abs(s.charAt(right) - t.charAt(right));
+
+            while(cost > maxCost){
+                cost -= Math.abs(s.charAt(left)  - t.charAt(left));
+                left++;
+            }
+            maxLen =Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
+
