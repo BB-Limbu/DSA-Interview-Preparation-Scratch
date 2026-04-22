93c5c0e9f0388c4f45e210350845c93dd6f3c5a0
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-04-22 16:10:19 +0530

    Remove Kth Digits

---
+++ Array/String/RemoveKthDigits.java
@@ -0,0 +1,40 @@
+
+
+public class RemoveKthDigits {
+    public static String removeKthDigit(String s, int k){
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0 ; i < s.length(); i++) {
+            char digit = s.charAt(i);
+
+            while (!st.isEmpty() && k > 0 && st.peek() > digit) {
+                st.pop();
+                k--;
+            }
+            st.push(digit);
+        }
+        while(k > 0){
+            st.pop();
+            k--;
+        }
+
+        if(st.isEmpty()){
+            return "0";
+        }
+
+        StringBuilder res = new StringBuilder();
+        while(!st.isEmpty()){
+            res.append(st.pop());
+        }
+        while(res.length() > 0 && res.charAt(res.length()-1) == '0'){
+            res.deleteCharAt(res.length()-1);
+        }
+        res.reverse();
+        if(res.length() == 0){
+            return "0";
+        }
+        return res.toString();
+
+    }
+}
+
