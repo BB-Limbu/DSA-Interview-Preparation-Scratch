e3b779e7f43e2d306fb80444ecd2c36f02c55273
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-04-28 22:12:31 +0530

    Topological Sort in Graph

---
+++ Array/TopologicalSortingGraph.java
@@ -0,0 +1,50 @@
+
+
+public class TopologicalSortingGraph {
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge>[] graph){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[2].add(new Edge(2,3));
+        graph[3].add(new Edge(3,1));
+        graph[4].add(new Edge(4,0));
+        graph[4].add(new Edge(4,1));
+        graph[5].add(new Edge(5,0));
+        graph[5].add(new Edge(5,2));
+    }
+    public static void topologicalSort(ArrayList<Edge>[] graph){
+        boolean[] vis = new boolean[graph.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < graph.length; i++){
+            if(!vis[i]){
+                topSortUtil(graph,i,vis,st); // dfs
+            }
+        }
+        while(!st.isEmpty()){
+            System.out.print(st.pop()+" ");
+        }
+    }
+    public static  void topSortUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis, Stack<Integer> st){
+        vis[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]){
+                topSortUtil(graph, e.dest, vis, st);
+            }
+        }
+        st.push(curr);
+    }
+}
+
