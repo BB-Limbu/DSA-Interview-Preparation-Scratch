077b747c2515222f60db673a9e0059959175c5d1
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-05-03 22:15:19 +0530

    Sum Prime Between Numbers

---
+++ Array/SumPrimeNum.java
@@ -0,0 +1,34 @@
+public class SumPrimeNum {
+        public int reverse(int n) {
+            int rev = 0;
+            while (n > 0) {
+                rev = rev * 10 + (n % 10);
+                n /= 10;
+            }
+            return rev;
+        }
+
+        public boolean isPrime(int num) {
+            if (num < 2) return false;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+        public int sumOfPrimesInRange(int n) {
+            int r = reverse(n);
+
+            int low = Math.min(n, r);
+            int high = Math.max(n, r);
+
+            int sum = 0;
+
+            for (int i = low; i <= high; i++) {
+                if (isPrime(i)) {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
