4409eed6c04a31410d50aaa2de770f4a34e554d5
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-05-27 22:15:51 +0530

    Find Content Children

---
+++ Array/FindContentChildren.java
@@ -0,0 +1,22 @@
+class FindContentChildren {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+
+            // Cookie satisfies child
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+
+            cookie++;
+        }
+
+        return child;
+    }
+
+}
