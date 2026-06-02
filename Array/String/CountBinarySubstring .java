70ef538f595a18bb2114ecaf83035d357e3b0cbd
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-06-02 22:06:46 +0530

    Count Binary Substring

---
+++ Array/String/CountBinarySubstring .java
@@ -0,0 +1,26 @@
+
+
+class CountBinarySubstring {
+    public static  int countBinarySubStr(String s){
+        int res = 0, prev = 0, count = 1;
+
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                count++;
+            }
+            else{
+                prev = count;
+                count = 1;
+            }
+            if(count <= prev){
+                res++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        String s = "00110011";
+        System.out.println(countBinarySubStr(s));
+    }
+}
+
