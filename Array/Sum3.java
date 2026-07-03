e0703c32a00a19c2816196d6459d5421fc756f5b
BB-Limbu <balbahadurlimbu404@gmail.com>
2026-07-03 22:38:18 +0530

    3Sum

---
+++ Array/Sum3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Sum3 {
+    public  static List<List<Integer>> ThreeSum(int[] nums){
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < n-2; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i+1;
+            int right = n-1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    //Ignore duplicate
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right -1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if(sum > 0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        int[] arr = {-1,0,1,2,-1,-4};
+        List<List<Integer>> ans = ThreeSum(arr);
+        for(int i = 0; i < ans.size(); i++){
+            System.out.print(ans.get(i) +" ");
+        }
+        System.out.println();
+    }
+}
